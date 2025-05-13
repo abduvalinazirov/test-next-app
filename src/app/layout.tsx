f1bd7195618d3f09923ba45a42a73d5f6d4a0db4
devHikmat <devhikmatgithub@gmail.com>
2025-05-13 20:19:31 +0500

    header changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <h1>header</h1>
+        <h1>New header</h1>
         {children}
         <h1>Footer</h1>
       </body>
